be7174e754963a89958b0db1afec15e6725d46a2
Bruno Ribeiro <brunz36@gmail.com>
2017-03-21 16:17:35 -0400

    Added a second method to make the guesses array

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,4 +1,4 @@
-random_number = Random.rand(1..10)
+random_number = Random.rand(1..100)
 
 def user_guess(prompt)
   print prompt + ": "
@@ -7,12 +7,17 @@ end
 
 guess_array = []
 
-loop do
-  guess = user_guess("Please enter a number between 1 and 10")
+def guess_array_method(guess_array, guess)
   guess_array.push(guess)
+end
+
+
+loop do
+  guess = user_guess("Please enter a number between 1 and 100")
+  guess_array_method(guess_array, guess)
 
   if guess_array.length == 5
-    puts "too many"
+    puts "Sorry but you've ran out of guesses, the correct number was #{random_number}."
     break
   end
 
@@ -20,8 +25,8 @@ loop do
     puts "Correct, then #{random_number} is the same as #{guess}."
     break
   elsif guess < random_number
-    puts "Your number #{guess}, is less than the number you're searching for.#{random_number}"
+    puts "Your number #{guess}, is less than the number you're searching for."
   elsif guess > random_number
-    puts "Your number #{guess}, is more than the number you're searching for.#{random_number}"
+    puts "Your number #{guess}, is more than the number you're searching for."
   end
 end
