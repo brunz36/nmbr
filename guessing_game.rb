0c278e53ea3ad264e85969757a35c752de342c53
Bruno Ribeiro <brunz36@gmail.com>
2017-03-21 14:55:22 -0400

    Simplified my code and loop do for matching guess

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,5 +1,19 @@
-def random_number_generator
-  Random.rand(1...101)
+random_number = Random.rand(1...11)
+
+def user_guess(prompt)
+  print prompt + ": "
+  gets.chomp.to_i
 end
 
-puts random_number_generator
+
+guess = user_guess("Please enter a number between 1 and 10")
+
+loop do
+  if guess == random_number
+    puts "Correct, then #{random_number} is the same as #{guess}."
+  elsif guess < random_number
+    puts "Your number #{guess}, is less than the number you're searching for.#{random_number}"
+  elsif guess > random_number
+    puts "Your number #{guess}, is more than the number you're searching for.#{random_number}"
+  end
+end
