cbec2141e11d8159f57299ef1e2268c8d237bbc5
Bruno Ribeiro <brunz36@gmail.com>
2017-03-21 15:05:11 -0400

    Finished loop do(with your help), need to add another method.

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,16 +1,24 @@
-random_number = Random.rand(1...11)
+random_number = Random.rand(1..10)
 
 def user_guess(prompt)
   print prompt + ": "
   gets.chomp.to_i
 end
 
-
-guess = user_guess("Please enter a number between 1 and 10")
+guess_array = []
 
 loop do
+  guess = user_guess("Please enter a number between 1 and 10")
+  guess_array.push(guess)
+
+  if guess_array.length == 5
+    puts "too many"
+    break
+  end
+
   if guess == random_number
     puts "Correct, then #{random_number} is the same as #{guess}."
+    break
   elsif guess < random_number
     puts "Your number #{guess}, is less than the number you're searching for.#{random_number}"
   elsif guess > random_number
